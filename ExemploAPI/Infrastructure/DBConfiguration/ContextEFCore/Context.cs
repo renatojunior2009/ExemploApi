bd48b674a229f3477b768eb80c6caa51ddd64f80
Renato Júnior <renatojunior2009@gmail.com>
2020-09-02 16:15:46 -0300

    - Inclusão do construtor padrão e do metodo OnConfiguring.

--- ExemploAPI/Infrastructure/DBConfiguration/ContextEFCore/Context.cs
+++ ExemploAPI/Infrastructure/DBConfiguration/ContextEFCore/Context.cs
@@ -1,16 +1,28 @@
 ﻿using Domain;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace Infrastructure.DBConfiguration.ContextEFCore
 {
     public class Context : DbContext
     {
         #region Publics Properties 
-        public DbSet<Client> Client { get; set; } 
+        public DbSet<Client> Client { get; set; }
         #endregion
 
         #region Constructor
-        public Context(DbContextOptions<Context> options) : base(options) { } 
+        public Context() { }
+        public Context(DbContextOptions<Context> options) : base(options) { }
+        #endregion
+
+        #region Methods
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(DatabaseConnection.ConnectionConfiguration.GetConnectionString("DefaultConnection"));
+            }
+        } 
         #endregion
     }
 }
