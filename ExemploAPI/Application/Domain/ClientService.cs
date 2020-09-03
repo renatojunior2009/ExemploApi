d777238820d97324ebe6dd80e8daa5dc3fbf80a3
Renato Júnior <renatojunior2009@gmail.com>
2020-09-03 16:49:54 -0300

    - Criação da classe de serviço especifica do cliente.

---
+++ ExemploAPI/Application/Domain/ClientService.cs
@@ -0,0 +1,25 @@
+﻿using Application.Base;
+using Application.Interfaces.Domain;
+using Domain;
+using Infrastructure.Interfaces.Repositories.Domain;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Application.Domain
+{
+    public class ClientService: ServiceBase<Client>, IClientService
+    {
+        private readonly IClientRepository _repository;
+        public ClientService(IClientRepository repository):base(repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<IEnumerable<Client>> GetSpecialsCustomers()
+        {
+            return await _repository.GetSpecialsCustomers();
+        }
+
+        
+    }
+}
