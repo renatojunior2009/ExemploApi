61a9d21e9b59a8cd70b948c09eb7365b9a339bba
Renato Júnior <renatojunior2009@gmail.com>
2020-09-03 10:33:16 -0300

    - Implementação do Domain Repository

---
+++ ExemploAPI/Infrastructure/Repositories/Domain/DomainRepository.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Entities.Base;
+using Infrastructure.Interfaces.Repositories.Domain;
+using Infrastructure.Repositories.Base;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories.Domain
+{
+    public class DomainRepository<T>: RepositoryBaseAsync<T>, IDomainRepository<T> where T: class, IEntityBase
+    {
+        #region Constructor
+        public DomainRepository(DbContext dbContext) : base(dbContext) { } 
+        #endregion
+    }
+}
