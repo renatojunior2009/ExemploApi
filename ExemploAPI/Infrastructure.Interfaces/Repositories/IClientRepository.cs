0f01b33dcb586410020050f7ce0dc61a50c4a56a
Renato Júnior <renatojunior2009@gmail.com>
2020-09-03 08:15:28 -0300

    - Inclusão da Interface Especifica de Clientes.

--- ExemploAPI/Infrastructure.Interfaces/Repositories/IClientRepository.cs
+++ ExemploAPI/Infrastructure.Interfaces/Repositories/IClientRepository.cs
@@ -1,9 +1,12 @@
 ﻿using Domain;
 using Infrastructure.Interfaces.Repositories.Domain;
+using System.Collections;
+using System.Threading.Tasks;
 
 namespace Infrastructure.Interfaces.Repositories
 {
     public interface IClientRepository : IDomainRepository<Client>
     {
+        Task<IEnumerable> GetSpecialsCustomers();
     }
 }
