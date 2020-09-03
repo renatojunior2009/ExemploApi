7e17b9a871f3b5009b728f1125077da453098c7c
Renato Júnior <renatojunior2009@gmail.com>
2020-09-03 16:40:01 -0300

    - Criação da implementação do Servico Base da aplicação.

---
+++ ExemploAPI/Application/Base/ServiceBase.cs
@@ -0,0 +1,29 @@
+﻿using Application.Interfaces.Base;
+using Domain.Entities.Base;
+using Infrastructure.Interfaces.Repositories.Base;
+using System.Threading.Tasks;
+
+namespace Application.Base
+{
+    public class ServiceBase<T> : IServiceBase<T> where T : class, IEntityBase
+    {
+        #region Fields
+        protected readonly IRepositoryBaseAsync<T> _repository;
+        #endregion
+
+        #region Constructor
+        public ServiceBase(IRepositoryBaseAsync<T> repository)
+        {
+            _repository = repository;
+        }
+        #endregion
+
+        #region Public Methods
+        public async Task<T> SaveAsync(T entity)
+        {
+            return await _repository.SaveAsync(entity);
+        } 
+
+        #endregion
+    }
+}
