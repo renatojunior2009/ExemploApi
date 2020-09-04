c302c6e432b72c86140c3e34fba5c44b9d830da1
Renato Júnior <renatojunior2009@gmail.com>
2020-09-04 11:23:27 -0300

    - Implementação do serviço que ira retornar os clientes especiais do banco de dados. Alteração do LaunchSettings.

--- ExemploAPI/ExemploAPI/Controllers/ClientController.cs
+++ ExemploAPI/ExemploAPI/Controllers/ClientController.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Domain;
+using Application.Interfaces.Domain;
+
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace ExemploAPI.Controllers
 {
@@ -6,5 +11,17 @@ namespace ExemploAPI.Controllers
     [Route("[controller]")]
     public class ClientController : ControllerBase
     {
+        private readonly IClientService _clientService;
+
+        public ClientController(IClientService clientService)
+        {
+            _clientService = clientService;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Client>> GetSpecialCustomers()
+        {
+            return await _clientService.GetSpecialsCustomers();
+        }
     }
 }
