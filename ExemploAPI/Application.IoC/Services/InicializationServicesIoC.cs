0a145b0e971fbad84e31c55ed7824d5811696986
Renato Júnior <renatojunior2009@gmail.com>
2020-09-04 08:17:13 -0300

    - Implementação da classe ServicesIoC.

---
+++ ExemploAPI/Application.IoC/Services/InicializationServicesIoC.cs
@@ -0,0 +1,17 @@
+﻿using Application.Domain;
+using Application.Interfaces.Base;
+using Application.Interfaces.Domain;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Application.IoC.Services
+{
+
+    public static class ServicesIoc
+    {
+        public static void StartupsDependenciesServices(this IServiceCollection services)
+        { 
+            services.AddScoped(typeof(IServiceBase<>), typeof(IServiceBase<>));
+            services.AddScoped<IClientService, ClientService>();
+        }
+    }
+}
