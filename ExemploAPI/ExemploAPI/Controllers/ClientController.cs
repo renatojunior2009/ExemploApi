2eb039e0ebd8ac7ecb19c359273ede8f60576e26
Renato Júnior <renatojunior2009@gmail.com>
2020-09-04 11:07:26 -0300

    - Inclusão da classe de ClienteController que ira expor os metodos para consumo.

---
+++ ExemploAPI/ExemploAPI/Controllers/ClientController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ExemploAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ClientController : ControllerBase
+    {
+    }
+}
